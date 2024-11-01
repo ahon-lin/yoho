e2d2f4b0884790cc11958b23504265a697903229
ahon <ahon.lin@mecotech.com.tw>
2024-11-01 10:02:57 +0800

    change: js改用方法引用

---
+++ js/scripts-loader.js
@@ -0,0 +1,24 @@
+// scripts-loader.js
+const scripts = [
+  "https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js",
+  "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js",
+  "./js/custom.js",
+  "./js/bootstrap-submenu.js",
+  "./js/aos.js",
+  "./js/ready.js"
+];
+
+scripts.forEach(function(src) {
+  let script;
+  if (src.endsWith(".js")) {
+    script = document.createElement("script");
+    script.src = src;
+  } else if (src.endsWith(".css")) {
+    script = document.createElement("link");
+    script.href = src;
+    script.rel = "stylesheet";
+  } else {
+    return; // 跳過未知的文件類型
+  }
+  document.head.appendChild(script);
+});
