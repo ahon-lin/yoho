e051d8eb60f220e2697167b7603d59dad3eea074
Fourth <ddnstarAhora@gmail.com>
2024-12-02 11:12:44 +0800

    feat: 醫療團隊頁面新增/醫療團隊頁面響應式圖像完成

--- js/custom.js
+++ js/custom.js
@@ -1,74 +1,88 @@
-$(function(){
-	$("#header").load("./commom/header.html", function() {
-		$("#footer").load("./commom/footer.html", function() {
-			if($('#form-content').length){
-				$("#form-content").load("./commom/form.html", function() {			
+$(function () {
+	$("#header").load("./commom/header.html", function () {
+		$("#footer").load("./commom/footer.html", function () {
+			if ($('#form-content').length) {
+				$("#form-content").load("./commom/form.html", function () {
 					jsInit();
-			    });
-			}else{
+				});
+			} else {
 				jsInit();
 			}
-	    });
-    });
+		});
+	});
 })
 
-function jsInit(){
+// document.addEventListener("DOMContentLoaded",function(){
+// 	$("#header").load("./commom/header.html", function () {
+// 		$("#footer").load("./commom/footer.html", function () {
+// 			if ($('#form-content').length) {
+// 				$("#form-content").load("./commom/form.html", function () {
+// 					jsInit();
+// 				});
+// 			} else {
+// 				jsInit();
+// 			}
+// 		});
+// 	});
+// })
+
+function jsInit() {
 	AOS.init({
-	    once: true,
+		once: true,
 	});
-	$('.menu-btn').on('click', function(){
+	$('.menu-btn').on('click', function () {
 		$('#hamburger-1').toggleClass('active');
 	});
 	if (window.location.hash) {
 		let target = $(window.location.hash);
 		if (target.length) {
 			let height = 80;
-			if($(target).offset().top >= 100) height = 50;
+			if ($(target).offset().top >= 100) height = 50;
 			$('html, body').animate({
 				scrollTop: target.offset().top - height
 			}, 800);
-		}else{
-			if($('.header-top-bar').offset().top >= 100) add_autoHeader();
+		} else {
+			if ($('.header-top-bar').offset().top >= 100) add_autoHeader();
 		}
-	}else{
-		if($('.header-top-bar').offset().top >= 100) add_autoHeader();
+	} else {
+		if ($('.header-top-bar').offset().top >= 100) add_autoHeader();
 	}
-	$('a[href*="#"]').on('click', function(e){
+	$('a[href*="#"]').on('click', function (e) {
 		let target = $(this.hash);
 		if ($(target).length) {
 			e.preventDefault();
 			let height = 80;
-			if($(target).offset().top >= 100) height = 50;
+			if ($(target).offset().top >= 100) height = 50;
 			$('html, body').animate({
-			  scrollTop: $(target).offset().top - height
+				scrollTop: $(target).offset().top - height
 			}, 800);
 		}
 	});
-	$(window).scroll(function() {
-		if($('.header-top-bar').offset().top >= 100){
+	$(window).scroll(function () {
+		if ($('.header-top-bar').offset().top >= 100) {
 			add_autoHeader();
-		}else{
+		} else {
 			remove_autoHeader();
 		}
 	});
 
-	$(window).on('resize', function(){ 
-		navLinkAttr(); 
+	$(window).on('resize', function () {
+		navLinkAttr();
 	})
 
-	$('[data-submenu]').submenupicker(); 
+	$('[data-submenu]').submenupicker();
 	navLinkAttr();
 }
 
-function navLinkAttr(){
-	if($(window).width() > 991) $('.nav-click').attr('data-toggle','');
-	else $('.nav-click').attr('data-toggle','dropdown');
+function navLinkAttr() {
+	if ($(window).width() > 991) $('.nav-click').attr('data-toggle', '');
+	else $('.nav-click').attr('data-toggle', 'dropdown');
 }
 
-function add_autoHeader(){
-	if(!$('.header-top-bar').hasClass('active')) $('.header-top-bar').addClass('active');
+function add_autoHeader() {
+	if (!$('.header-top-bar').hasClass('active')) $('.header-top-bar').addClass('active');
 }
 
-function remove_autoHeader(){
-	if($('.header-top-bar').hasClass('active')) $('.header-top-bar').removeClass('active');
+function remove_autoHeader() {
+	if ($('.header-top-bar').hasClass('active')) $('.header-top-bar').removeClass('active');
 }
--- js/scripts-loader.js
+++ js/scripts-loader.js
@@ -1,7 +1,6 @@
 // scripts-loader.js
 const scripts = [
   "https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js",
-  "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js",
   "./js/custom.js",
   "./js/bootstrap-submenu.js",
   "./js/aos.js",
